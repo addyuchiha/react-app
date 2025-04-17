d0e0bc7d3cb59171e3f412e173629289b15a7c34
addyuchiha <addyfitz0@gmail.com>
2025-04-17 18:24:58 +0530

    add hero

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,36 +4,36 @@ const Navbar = () => {
   return (
     <>
       <header className="flex justify-center">
-        <nav className="bg-primary grid grid-cols-3 text-white mx-auto mt-2 p-4 rounded-2xl items-center fixed w-full max-w-4xl">
+        <nav className="bg-primary grid grid-cols-3 text-textLight  mt-4 p-4 rounded-2xl items-center fixed w-full max-w-5xl mx-16">
           <div className="text-2xl font-black">LOGO</div>
           <ul className="grid grid-cols-4 space-x-1 text-sm       items-center">
             <a
               href="#"
-              className="text-center hover:text-gray-300 transition-colors"
+              className="text-center hover:brightness-90 transition-all"
             >
               Home
             </a>
             <a
               href="#"
-              className="text-center hover:text-gray-300 transition-colors"
+              className="text-center hover:brightness-90 transition-all"
             >
               Pricing
             </a>
             <a
               href="#"
-              className="text-center hover:text-gray-300 transition-colors"
+              className="text-center hover:brightness-90 transition-all"
             >
               About
             </a>
             <a
               href="#"
-              className="text-center hover:text-gray-300 transition-colors"
+              className="text-center hover:brightness-90 transition-all"
             >
               Contact
             </a>
           </ul>
           <div className="flex justify-end">
-            <Button />
+            <Button children={undefined} color={"accent"} />
           </div>
         </nav>
       </header>
