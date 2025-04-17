adefc669e03ca43aaa4cdc2d53764eb0a65dcff5
addyuchiha <addyfitz0@gmail.com>
2025-04-17 16:46:50 +0530

    make navbar expand according to screen width

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <>
       <header className="flex justify-center">
-        <nav className="bg-primary grid grid-cols-3 text-white max-w-3xl mx-auto mt-2 p-4 rounded-2xl items-center fixed">
+        <nav className="bg-primary grid grid-cols-3 text-white mx-auto mt-2 p-4 rounded-2xl items-center fixed w-full max-w-4xl">
           <div className="text-2xl font-black">LOGO</div>
           <ul className="grid grid-cols-4 space-x-1 text-sm       items-center">
             <a
