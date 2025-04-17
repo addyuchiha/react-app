88811354c411f2519e7f09fc7966f86bc9bb31f2
addyuchiha <addyfitz0@gmail.com>
2025-04-17 16:01:51 +0530

    Add Navbar

---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Button = () => {
+    return (
+        <button className="bg-accent px-4 py-3 rounded-lg flex space-x-2 hover:bg-opacity-70 transition-all">
+            <span>Get Started</span>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-6"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                />
+            </svg>
+        </button>
+    );
+};
+
+export default Button;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import Button from "./Button";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+    return (
+        <>
+            <header>
+                <nav className="bg-primary grid grid-cols-3 text-white max-w-3xl mx-auto mt-2 p-4 rounded-2xl items-center">
+                    <div className="text-2xl font-black">LOGO</div>
+                    <ul className="grid grid-cols-4 space-x-1 text-sm       items-center">
+                        <a
+                            href="#"
+                            className="text-center hover:text-gray-300 transition-colors"
+                        >
+                            Home
+                        </a>
+                        <a
+                            href="#"
+                            className="text-center hover:text-gray-300 transition-colors"
+                        >
+                            Pricing
+                        </a>
+                        <a
+                            href="#"
+                            className="text-center hover:text-gray-300 transition-colors"
+                        >
+                            About
+                        </a>
+                        <a
+                            href="#"
+                            className="text-center hover:text-gray-300 transition-colors"
+                        >
+                            Contact
+                        </a>
+                    </ul>
+                    <div className="flex justify-end">
+                        <Button />
+                    </div>
+                </nav>
+            </header>
+        </>
+    );
+};
 
-export default Navbar
+export default Navbar;
