687cc6a3788cedb0f01106c536f5815dcc0be522
addyuchiha <addyfitz0@gmail.com>
2025-04-21 09:51:51 +0530

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { Router } from "lucide-react";
 import HomePage from "./pages/HomePage";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
+const router = createBrowserRouter([{
+    path: '/',
+    element: <HomePage />
+},
+{
+    path: '/login',
+    element: <Login />
+},
+{
+    path: '/sign-up',
+    element: <SignUp />
+}])
 
 function App() {
     return (
         <>
-            <HomePage />
+            <RouterProvider router={router} />
         </>
     );
 }
