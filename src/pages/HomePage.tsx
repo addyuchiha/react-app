09558c53b5d67f0b850414bd46666763983677aa
addyuchiha <addyfitz0@gmail.com>
2025-04-21 09:56:43 +0530

    add footer

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,18 +4,20 @@ import HowToUse from "../components/HowToUse/HowToUse";
 import Features from "../components/Features/Features";
 import Pricing from "../components/Pricing/Pricing";
 import ContactUs from "../components/ContactUs";
+import Footer from "../components/Footer/Footer";
 
 const HomePage = () => {
   return (
-    <div className="bg-bgLight px-16">
+    <div className="bg-bgLight">
       <Navbar />
-      <main>
+      <main className="md:px-16 px-5">
         <Hero />
         <HowToUse />
         <Features />
         <Pricing />
         <ContactUs />
       </main>
+      <Footer />
     </div>
   );
 };
