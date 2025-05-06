86b31754c33a48c2a12d48ef4a065c2188cba47a
addyuchiha <addyfitz0@gmail.com>
2025-05-06 13:43:35 +0530

    feat: update Template component to accept children and handle optional heading

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -1,16 +1,21 @@
+import { ReactNode } from "react";
 import Sidebar from "./Template/Sidebar";
 
 interface Props {
-    heading: string
+  heading: string | undefined;
+  children: ReactNode;
 }
 
-function Template({heading}: Props) {
+function Template({ heading, children }: Props) {
   return (
     <div className="flex bg-bgLight">
       <Sidebar />
       <div className="w-full">
-        <div>
-            <span className="text-6xl font-bold">{heading}</span>
+        <div className="p-4 pl-0 space-y-4 h-screen flex flex-col">
+          {heading ? (
+            <span className="text-6xl font-bold block">{heading}</span>
+          ) : undefined}
+          {children}
         </div>
       </div>
     </div>
