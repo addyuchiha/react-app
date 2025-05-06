a50c39a865b526b4c43b28efe8f3bc81e28eef2d
addyuchiha <addyfitz0@gmail.com>
2025-05-06 13:54:50 +0530

    feat: implement sign-out functionality in Profile Details component

--- src/components/Profile/Details.tsx
+++ src/components/Profile/Details.tsx
@@ -1,8 +1,17 @@
+import Cookies from "js-cookie";
 import useState from "../../scripts/auth/useState";
 import { LogOut, User2 } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export default function Details() {
   const user = useState();
+  const navigate = useNavigate();
+
+  const signOut = () => {
+    Cookies.remove("accessToken");
+    Cookies.remove("refreshToken");
+    navigate("/sign-in");
+  };
 
   return (
     <div className="bg-primary p-8 rounded-xl flex flex-grow flex-col">
@@ -17,9 +26,13 @@ export default function Details() {
           <span className="font-thin">{user.email}</span>
         </div>
       </div>
-      <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition-colors font-medium flex justify-center items-center gap-2 mt-auto">
+      <div className="h-[1px] bg-accent/20 my-8" />
+      <button
+        onClick={signOut}
+        className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition-colors font-medium flex justify-center items-center gap-2 mt-auto"
+      >
         <LogOut size={18} />
-        Logout
+        SignOut
       </button>
     </div>
   );
