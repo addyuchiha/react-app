4481500cff3c87124462982a76c0cb5cd93c5a81
addyuchiha <addyfitz0@gmail.com>
2025-05-09 17:50:27 +0530

    feat: add noTitle prop to Pricing component for conditional rendering of title; update PricingDecorator and HomePage accordingly

--- src/components/Pricing/PricingDecorator.tsx
+++ src/components/Pricing/PricingDecorator.tsx
@@ -8,9 +8,9 @@ interface Props {
 function PricingDecorator({ children }: Props) {
   return (
     <div>
-      <div className="absolute w-screen h-screen backdrop-blur z-20 flex flex-col justify-center items-center space-y-4">
-        <div className="mt-auto w-full">
-          <Pricing />
+      <div className="absolute w-screen h-screen backdrop-blur z-20 flex flex-col justify-center items-center">
+        <div className="w-full px-10">
+          <Pricing noTitle={true} />
         </div>
       </div>
       <div className="z-10">
