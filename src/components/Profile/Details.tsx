8e93257af21d5f42e551918af62248356977ffb0
Krishna <kg9977553311@gmail.com>
2025-06-21 13:07:40 +0530

    fixed bugs

--- src/components/Profile/Details.tsx
+++ src/components/Profile/Details.tsx
@@ -40,9 +40,9 @@ export default function Details() {
 
       <button
         onClick={forgetPassword}
-        className="text-white hover:text-accent px-4 py-1 rounded-lg transition-colors font-medium flex justify-center items-center gap-2"
+        className="text-white hover:text-accent px-4 py-1 rounded-lg transition-colors font-medium text-sm flex justify-center items-center gap-2"
       >
-        Reset Password???
+        Reset Password
       </button>
 
       <button
